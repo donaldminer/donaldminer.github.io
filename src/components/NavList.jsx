3ca318ad33e8ffdd700f70ca80284f8e79df4be6
donaldminer <drminer@dons.usfca.edu>
2024-08-23 09:55:51 -0700

    Settling on single Pane open for now

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -2,7 +2,7 @@ import { About, Portfolio, Timeline, Contact } from "./index.js";
 import { useState } from "react";
 
 export default function NavList() {
-  const [activePanes, setActivePanes] = useState([]);
+  const [activePane, setActivePane] = useState();
 
   const items = [
     { id: 1, text: "About", pane: <About /> },
@@ -11,11 +11,7 @@ export default function NavList() {
     { id: 4, text: "Contact", pane: <Contact /> },
   ];
   const handleClick = (pane) => {
-    if (activePanes.includes(pane)) {
-      setActivePanes(activePanes.filter((item) => item !== pane));
-    } else {
-      setActivePanes([...activePanes, pane]);
-    }
+    setActivePane(pane);
   };
   return (
     <>
@@ -32,7 +28,7 @@ export default function NavList() {
       </ul>
       <section className="col-span-9 row-span-10 grid-cols-6 grid-rows-6">
         <div className="flex-1 flex-row bg-black bg-opacity-65 backdrop-blur-sm">
-          {activePanes}
+          {activePane}
         </div>
       </section>
     </>
