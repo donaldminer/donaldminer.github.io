988053c976d52ca9c317fe1cc6d95171be02bb02
donaldminer <drminer@dons.usfca.edu>
2024-08-24 15:47:29 -0700

    mobile layout for portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,7 @@ import PortfolioItem from "./PortfolioItem";
 
 export default function Portfolio() {
   return (
-    <div className="grid flex-1 grid-flow-row grid-cols-3 place-content-evenly gap-4 p-2 sm:grid-flow-col sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid flex-1 grid-flow-col grid-cols-3 place-content-evenly gap-4 p-2 sm:grid-flow-row sm:grid-cols-1 sm:grid-rows-3 md:grid-cols-2 lg:grid-cols-3">
       {portfolio.map((item) => (
         <PortfolioItem
           key={item.title}
