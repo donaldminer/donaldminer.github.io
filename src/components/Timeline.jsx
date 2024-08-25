da7fab2efd9de403dcbd7f29ccae0597c7acd480
donaldminer <drminer@dons.usfca.edu>
2024-08-25 11:36:29 -0700

    Timeline Scroll styling

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,16 +3,18 @@ import TimelineItem from "./TimelineItem";
 
 export default function Timeline() {
   return (
-    <div className="flex-1 flex-row bg-black bg-opacity-65 backdrop-blur-sm">
-      {timeline.map((item) => (
-        <TimelineItem
-          key={item.title}
-          year={item.year}
-          title={item.title}
-          duration={item.duration}
-          details={item.details}
-        />
-      ))}
+    <div className="flex h-5/6 justify-center">
+      <div className="w-3/4 appearance-none overflow-y-scroll text-pretty bg-black bg-opacity-65 backdrop-blur-sm">
+        {timeline.map((item) => (
+          <TimelineItem
+            key={item.title}
+            year={item.year}
+            title={item.title}
+            duration={item.duration}
+            details={item.details}
+          />
+        ))}
+      </div>
     </div>
   );
 }
