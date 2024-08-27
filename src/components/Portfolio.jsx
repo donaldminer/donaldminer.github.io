4234bbb548f37f92b242ca1a9fd88543e7caabca
donaldminer <drminer@dons.usfca.edu>
2024-08-27 12:08:49 -0700

    Fixed Thumbnail issue

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,23 +25,27 @@ function PortfolioItem({ imgUrl, stack, title, workUrl, description }) {
       href={workUrl}
       target="_blank"
     >
-      <img
-        src={imgUrl}
-        alt="portfolio"
-        className="h-full w-full overflow-hidden object-cover object-center p-2"
-      />
-      <div className="flex h-full w-full flex-col place-content-between p-4">
-        <h3 className="text-start text-lg font-medium text-white sm:text-end">
+      <div>
+        <img
+          src={imgUrl}
+          alt="portfolio"
+          className="h-full w-full overflow-hidden object-cover object-center p-2"
+        />
+      </div>
+      <div className="grid h-full w-full place-content-between p-4">
+        <h3 className="text-start text-2xl font-medium text-white sm:text-end sm:text-lg">
           {title}
         </h3>
-        <p className="flex flex-row flex-wrap items-center justify-end gap-1 text-xs">
+        <p className="text-md flex flex-row flex-wrap items-center justify-center gap-1 sm:justify-end sm:text-xs">
           {stack.map((item) => (
             <span className="inline-block border-2 border-white border-opacity-50 bg-darkCream bg-opacity-70 px-2 py-1 text-white">
               {item}
             </span>
           ))}
         </p>
-        <p className="text-end text-xs">{description}</p>
+        <p className="text-md text-start sm:text-end sm:text-xs">
+          {description}
+        </p>
       </div>
     </a>
   );
